a65869da20280ebcc85f9b7eb7ea70c71f348311
anraku <anraku90@gmail.com>
2019-11-23 17:33:08 +0900

    create multi button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [ count, setCount ] = useState(0)
+
+  const f = () => setCount(n => n+1)
+  const x2 = () => setCount(n => n*2)
+  const divide3 = () => setCount(n => n%3 === 0? n/3 : n)
+  const reset = ()  => setCount(0)
   return (
-    <div>
-      This is a template for React app
-    </div>
+    <>
+      {count}
+      <button onClick={f}>count</button>
+      <button onClick={x2}>x2</button>
+      <button onClick={divide3}>divide3</button>
+      <button onClick={reset}>reset</button>
+    </>
   );
 }
 
