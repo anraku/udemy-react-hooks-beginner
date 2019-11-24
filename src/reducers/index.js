5f725820b4eaea8386f223a107243c41852f6c73
anraku <anraku90@gmail.com>
2019-11-24 15:40:28 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,23 @@
+import { strictEqual } from "assert";
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case 'CREATE_EVENT':
+      const id = state.length == 0 ? 1 : state[state.length - 1].id + 1 
+      return [
+        ...state,
+        {
+          id,
+          title: action.title,
+          body: state.body
+        }
+      ]
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENTS':
+      return []
+    default:
+      return state;
+  }
+}
+
+export default events;
