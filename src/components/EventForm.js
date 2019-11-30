1e5b50491668384c5b530bb2c9750bba3afbd743
anraku <anraku90@gmail.com>
2019-11-30 12:36:34 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,10 +1,14 @@
 import React, { useState, useContext } from 'react'
 import {
   CREATE_EVENT,
-  DELETE_ALL_EVENTS
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
 } from '../actions'
 import AppContext from '../contexts/AppContext'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { timeCurrentIso8601 } from '../utils'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import operationLogs from '../reducers/operationLogs'
 
 const EventForm = () => {
   const { state, dispatch }  = useContext(AppContext)
@@ -14,6 +18,11 @@ const EventForm = () => {
   const addEvent = e => {
     e.preventDefault()
     dispatch({type: CREATE_EVENT, title, body})
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    })
 
     setTitle('')
     setBody('')
@@ -21,7 +30,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault()
     const result = window.confirm('全てのイベントを本当に削除しても良いですか？')
-    if (result) dispatch({type: DELETE_ALL_EVENTS})
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS })
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: 'すべてのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
   const unCreatable = title === '' || body === ''
 
@@ -44,4 +60,4 @@ const EventForm = () => {
   )
 }
 
-export default EventForm;
+export default EventForm
